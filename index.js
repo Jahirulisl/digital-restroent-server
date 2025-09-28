f81114a88e9b0b1922ec0c028cef6ddc02332ddf
Jahirul Islam <nockjahirul@gmail.com>
2025-09-28 16:58:56 +0600

    make middle were api for varify token

--- index.js
+++ index.js
@@ -94,20 +94,38 @@ async function run() {
       res.send(result);
     })
     //for user admin make end>
-  
+
     //for jwt token api start 2
-    app.post('/jwt',async(req, res)=>{
-    const user = req.bady;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
-    res.send({token});
-  })
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
     //for jwt token api END 2
 
     //for make user data cellection and store api start
     const userCollection = client.db("digital-restruant").collection("users");
     //for api
+    //make middle weres for varify token start
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({messege: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1] ;
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbiden access'})
+        }
+       req.decoded = decoded;
+       next();
+      })
+      
+    }
+    //make middle weres for varify token end
+
     //for get user apistart
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
